32f7e60681810244b47880423c885c5b90186f32
kajela <titoyut56@gmail.com>
2023-04-14 03:48:53 -0400

    add the initial files

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ const cats = ['Milo', 'Otis', 'Garfield'];
 function destructivelyAppendCat(name){
     cats.push(name);
 }
-
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
